31aba5e7b0722e1606eaccaa23e5f1fb69e0ea1b
1Tech-X <30934772+1Tech-X@users.noreply.github.com>
2017-09-01 08:03:27 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -1,14 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo "-----------------------------------------------------"
-echo "SUBSCRIBE THE CHANNEL Tech-X"
-echo "-----------------------------------------------------"
+echo "##############################################"
+echo " SUBSCRIBE MY CHANNEL Tech-X "
+echo "##############################################"
 
-echo "Please Wait.........." 
+echo "WAIT UNTIL INSTALLING............" 
 
 echo "####################################"
 apt install autoconf bison clang coreutils curl findutils git apr figlet apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config postgresql-contrib wget make ruby-dev libgrpc-dev ncurses-utils termux-tools -y
 echo "####################################"
-echo "Downloading....."
+echo "Downloading & Extracting....."
 
 curl -L https://github.com/rapid7/metasploit-framework/archive/4.14.28.tar.gz | tar xz
 
@@ -17,27 +17,19 @@ cd metasploit-framework-4.14.28
 sed 's|git ls-files|find -type f|' -i metasploit-framework.gemspec
 
 sed -i 's/grpc (1.3.4)/grpc (1.4.1)/g' Gemfile.lock
+#Install bundler
 echo "Bundler is installing"
 gem install bundler
 
-
+#Install nokogiri
 echo "nokogiri is installing......"
 gem install nokogiri -- --use-system-libraries
 
+#Install Network-Interface
 
+gem install network_interface
 
-gem unpack network_interface
-cd network_interface-0.0.1
-sed 's|git ls-files|find -type f|' -i network_interface.gemspec
-curl -L https://wiki.termux.com/images/6/6b/Netifaces.patch -o netifaces.patch
-patch -p1 < netifaces.patch
-gem build network_interface.gemspec
-echo "network_interface is installing........"
-gem install network_interface-0.0.1.gem
-cd ..
-rm -r network_interface-0.0.1
-
-
+#Install gems
 gem unpack grpc -v 1.4.1
 cd grpc-1.4.1
 curl -LO https://raw.githubusercontent.com/grpc/grpc/v1.4.1/grpc.gemspec
@@ -49,27 +41,27 @@ gem install grpc-1.4.1.gem
 cd ..
 rm -r grpc-1.4.1
 
-
+#Bundle Install
 echo "bundle and all other dependencies are installing......"
 bundle install -j5
 
-
+#Fixing Shebang
 $PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
 
 cd metasploit-framework-4.14.28
 
 echo "###############################"
-echo "Original Work BY Vishalbiswani& Auxilus,Thanks  To  Vishalbiswani& Auxilus For The Code "
+echo "Thanks  To  Vishalbiswani& Auxilus "
 echo "###############################"
 figlet Tech-X
 echo "###############################################"
-echo "Subscribe The Channel TECHNICAL Tech-X"
-figlet SUBSCRIBE
+echo "Subscribe  My channel Tech-X"
+figlet Subscribe
 echo "###############################################"
 echo "###############################"
-echo "For HACKING & TECH VIDEOS"
+echo "For More Hacking video At Tech-X Channel"
 echo "###############################"
-figlet Tech-X
+figlet Tech-X 
 echo "####################################"
 echo " NOW YOU CAN RUN METASPLOIT BY JUST EXECUTE THE COMMAND :=>  ./msfconsole"
 echo "####################################"
